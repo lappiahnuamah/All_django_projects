94ea02c2225a93b0ad711ed65055415573411d8b
Lawrence <lappiahnuamah1999@gmail.com>
2021-04-26 14:35:11 +0000

    m

--- blog/resume/views.py
+++ blog/resume/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
-def greetings(request):
-    return HttpResponse('Hello World, My very first djano code')
+# def home(request):
+#     return HttpResponse('<h1>This is my Home Page</h1>')
+
+def home(request):
+    return render(request, 'resume/index.html')
