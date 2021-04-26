9821a20ce13a217be6481953a2d02c876a67c24a
Lawrence <lappiahnuamah1999@gmail.com>
2021-04-26 14:08:12 +0000

    New added

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from index import views
+from resume import views
 urlpatterns = [
     path('', views.greetings, name="greetings"),
     path('admin/', admin.site.urls),
