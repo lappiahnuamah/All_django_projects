3ae99dc0bd0c58c3abf1d9556daa7358fda662d5
Lawrence <lappiahnuamah1999@gmail.com>
2021-05-04 14:47:01 +0000

    final code

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+# from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.views import generic
 
 from .models import Post
 # Create your views here.
@@ -32,3 +35,11 @@ class BlogDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
+
+
+
+# Create your views here.
+# class SignUpView(generic.CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'signup.html'
